691261d8832ff03fa23ad1960722a697b57f5b36
xiaopin <251089096@qq.com>
2024-12-06 23:19:21 +0800

    add example

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/xiaopin/flutter/fvm/versions/3.24.5"
+export "FLUTTER_APPLICATION_PATH=/Users/xiaopin/Desktop/AppDev/Flutter/Projects/pp_kits/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/xiaopin/Desktop/AppDev/Flutter/Projects/pp_kits/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/xiaopin/Desktop/AppDev/Flutter/Projects/pp_kits/example/.dart_tool/package_config.json"
